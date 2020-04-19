81aaf4a76b9231fffa644c2072adffac0745ea54
AlanMaik <alanmaikm@gmail.com>
2020-04-19 12:04:30 -0300

    create discovery page

--- app/javascript/screens/discovery/index.js
+++ app/javascript/screens/discovery/index.js
@@ -1,9 +1,14 @@
 import React, { Fragment } from 'react'
 import NavbarFooter from '../../components/common/navbar_footer';
+import Discovery from '../../components/discovery'
+import SectionWrapper from '../../components/common/section_wrapper'
  
 const DiscoveryScreen = () => {
   return (
     <Fragment>
+      <SectionWrapper>
+        <Discovery />
+      </SectionWrapper>
       <NavbarFooter />
     </Fragment>
  
