340be2eb7895a4dce959169f41cf70d715adf90f
AlanMaik <alanmaikm@gmail.com>
2020-04-19 18:44:43 -0300

    Add and configure postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_04_13_235719) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -36,8 +39,8 @@ ActiveRecord::Schema.define(version: 2020_04_13_235719) do
   create_table "albums", force: :cascade do |t|
     t.string "title"
     t.datetime "date"
-    t.integer "category_id", null: false
-    t.integer "artist_id", null: false
+    t.bigint "category_id", null: false
+    t.bigint "artist_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["artist_id"], name: "index_albums_on_artist_id"
@@ -57,9 +60,9 @@ ActiveRecord::Schema.define(version: 2020_04_13_235719) do
   end
 
   create_table "favorites", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "favoritable_type", null: false
-    t.integer "favoritable_id", null: false
+    t.bigint "favoritable_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["favoritable_type", "favoritable_id"], name: "index_favorites_on_favoritable_type_and_favoritable_id"
@@ -67,8 +70,8 @@ ActiveRecord::Schema.define(version: 2020_04_13_235719) do
   end
 
   create_table "recently_heards", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "album_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "album_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["album_id"], name: "index_recently_heards_on_album_id"
@@ -78,7 +81,7 @@ ActiveRecord::Schema.define(version: 2020_04_13_235719) do
   create_table "songs", force: :cascade do |t|
     t.string "title"
     t.integer "played_count", default: 0
-    t.integer "album_id", null: false
+    t.bigint "album_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["album_id"], name: "index_songs_on_album_id"
